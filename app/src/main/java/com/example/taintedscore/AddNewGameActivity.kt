4a4abe5ea33eb123c40fc583f02ca14b19a41d60
Alexey <frostfireblood@gmail.com>
2021-12-07 05:24:34 +0300

    Сменил тип отображения получаемой даты на ресайклер, но он почему-то не биндит дату в сам ресайлер. AddNewGameActivity.kt 32 строка

--- app/src/main/java/com/example/taintedscore/AddNewGameActivity.kt
+++ app/src/main/java/com/example/taintedscore/AddNewGameActivity.kt
@@ -1,14 +1,15 @@
 package com.example.taintedscore
 
 import android.os.Bundle
-import android.widget.AutoCompleteTextView
-import android.widget.SimpleAdapter
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import com.example.taintedscore.data.NAME
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taintedscore.adapter.SearchAdapter
 import com.example.taintedscore.data.SearchResponseData
 import com.example.taintedscore.models.ExtViewModel
 
@@ -21,32 +22,19 @@ class AddNewGameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_new_game)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
-        val searchGame = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        val searchGame = findViewById<EditText>(R.id.searchBGG)
+        val recyclerSearchRes:RecyclerView = findViewById(R.id.recycler_for_search)
 
         extViewModel.value.searchLiveData.observe(this) {
-            val recList: List<Any> = it
-            val receivedList: List<SearchResponseData> = recList.filterIsInstance<SearchResponseData>()
-            var lisSearchResItems = receivedList.map { item ->
-                item.toMap()
-            }
-
-            val adapter = SimpleAdapter(
-                this,
-                lisSearchResItems,
-                android.R.layout.simple_dropdown_item_1line,
-                arrayOf(NAME),
-                intArrayOf(android.R.id.text1),
-            )
-
-            //val textView = findViewById<AutoCompleteTextView>(R.id.text)
-            searchGame.setAdapter(adapter)
-
-            searchGame.setOnFocusChangeListener { _, bool ->
-                if (bool) {
-                    searchGame.showDropDown()
-                }
-            }
             Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            val receivedList: List<SearchResponseData> = it.filterIsInstance<SearchResponseData>()
+
+            //вот же байнд
+            recyclerSearchRes.apply{
+                layoutManager= LinearLayoutManager(this@AddNewGameActivity)
+                adapter= SearchAdapter(receivedList)
+            }
+            recyclerSearchRes.adapter?.notifyDataSetChanged()
         }
 
         extViewModel.value.errorLiveData.observe(this) {
@@ -63,5 +51,29 @@ class AddNewGameActivity : AppCompatActivity() {
     }
 }
 
+
+/*
+           val recList: List<Any> = it
+           val receivedList: List<SearchResponseData> = recList.filterIsInstance<SearchResponseData>()
+           var lisSearchResItems = receivedList.map { item ->
+               item.toMap()
+           }
+
+           val adapter = SimpleAdapter(
+               this,
+               lisSearchResItems,
+               android.R.layout.simple_dropdown_item_1line,
+               arrayOf(NAME),
+               intArrayOf(android.R.id.text1),
+           )
+
+           val textView = findViewById<AutoCompleteTextView>(R.id.text)
+           textView.setAdapter(adapter)
+
+           textView.setOnFocusChangeListener { _, bool ->
+               if (bool) {
+                   textView.showDropDown()
+               }
+           }*/
 
 
